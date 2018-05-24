7ef4512fabb87908ca376d41ae8b5be6c1f1456f
Italo <italobarroscontato@gmail.com>
2018-05-23 22:22:11 -0300

    Segundo prototipo, restando implementar as funcoes wait e notify

--- src/Application/Aviao.java
+++ src/Application/Aviao.java
@@ -15,10 +15,12 @@ import java.util.logging.Logger;
  *
  * @author Ítalo
  */
-public class Aviao implements Runnable{
+public final class Aviao implements Runnable{
     Random aleatorio = new Random();
-    private String name;
-    public Aviao(String name, Pistas p1, Pistas p2){
+    private final String name;
+    private final boolean pertenceUm;
+    private boolean andamento;
+    public Aviao(String name, Pistas p1, Pistas p2){ 
         this.name = name;
         int ppista = aleatorio.nextInt(2);
         if(ppista == 0){
@@ -26,74 +28,107 @@ public class Aviao implements Runnable{
             System.out.println("O "+this.name+" irá para a "+p1.name);
         }else{
             p2.lista.add(this);
-            System.out.println("O "+this.name+"irá para a "+p2.name);
+            System.out.println("O "+this.name+" irá para a "+p2.name);
         }
-        new Thread(this, name).start();;
+        pertenceUm = verify(p1);
+        new Thread(this, name).start();
     }
 
-    
+    public boolean verify(Pistas p1){
+        return p1.lista.contains(this);
+    }
     public void manobra(){
         int r = aleatorio.nextInt(5) +3;
-        System.out.println("O "+this.name+" está manobrando. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" está manobrando. Duração :"+r+" segundos");
     }
     public void taxiar(){
         int r = aleatorio.nextInt(4) +2;
-        System.out.println("O "+this.name+" está taxiando. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" está taxiando. Duração :"+r+" segundos");
     }
     public void posicionar(){
         int r = aleatorio.nextInt(4) +1;
-        System.out.println("O "+this.name+" posicionou-se na cabeceira da pista. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" posicionou-se na cabeceira da pista. Duração :"+r+" segundos");
     }
     public void acelerar(){
-        int r = aleatorio.nextInt(3) +3;
-        System.out.println("O "+this.name+" acelerou e irá decolar. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        int r = aleatorio.nextInt(3) +3;   
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" acelerou e irá decolar. Duração :"+r+" segundos");
     }
     public void decolar(){
-        int r = aleatorio.nextInt(4) +4;
-        System.out.println("O "+this.name+" decolou. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        int r = aleatorio.nextInt(4) +4;   
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" decolou. Duração :"+r+" segundos");
     }
     public void afastar(){
-        int r = aleatorio.nextInt(5) +2;
-        System.out.println("O "+this.name+" afastou-se. Duração :"+r+" segundos");
-        /*try {
-            wait(r*1000);
+        int r = aleatorio.nextInt(5) +2;   
+        try {
+            Thread.sleep(r*1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
+        System.out.println("O "+this.name+" afastou-se. Duração :"+r+" segundos");
     }
-
-    @Override
-    public void run() {
+    public void exec(){
         this.manobra();
         this.taxiar();
         this.posicionar();
         this.acelerar();
         this.decolar();
-        this.afastar();
+        this.afastar(); 
+    }
+    public synchronized void execPistaUm(){
+           exec();
+           andamento = true;
+           notify();
+           while(andamento){
+               try {
+                   wait();
+               } catch (InterruptedException ex) {
+                   Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           }
+    }
+    public synchronized void execPistaDois(){
+            exec();
+            andamento = false;
+            notify();
+            while(andamento){
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Aviao.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+    }
+    @Override
+    public void run() {
+        if(pertenceUm){
+            execPistaUm();
+        }else{
+            execPistaDois();
+        }
     }
 }
